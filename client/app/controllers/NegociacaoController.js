d756a00cb072feb0626cbcf59766fcc4ce106e6b
WellitonBecker <welliton.lbecker@gmail.com>
2022-10-17 20:59:37 -0300

    video 11

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,17 +6,32 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQtde = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
 
-        let data       = DateConverter.paraData(this._inputData.value);
-        let negociacao = new Negociacao(data, parseInt(this._inputQtde.value), parseInt(this._inputValor.value));
+        this._negociacoes.adiciona(this._criaNegociacao());
 
-        console.log(DateConverter.paraTexto(negociacao.data));
-        // console.log(this._inputData.value);
-        // console.log(this._inputQtde.value);
-        // console.log(this._inputValor.value);
+        console.log(this._negociacoes.paraArray());
+
+        this._limparFormulario();
     };
+
+    _limparFormulario () {
+        this._inputData.value = '2022-01-01';
+        this._inputQtde.value = 1
+        this._inputValor.value = 2.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQtde.value),
+            parseFloat(this._inputValor.value)
+        );
+    }
+
 }
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -9,9 +9,9 @@ class DateConverter {
     }
 
     static paraData(texto) {
-    if (!/^\d{4}-\d{2}$/.test(texto)) {
-        throw new Error("Deve estar no formato aaaa-mm-dd");
-    }
+    // if (!/^\d{4}-\d{2}$/.test(texto)) {
+    //     throw new Error("Deve estar no formato aaaa-mm-dd");
+    // }
 
         return new Date(...texto.split('-').map((item, indice) => item - (indice % 2)));
     }
