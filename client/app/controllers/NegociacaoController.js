6434e0bfd76aac42b65db006d192dbdc66aa4513
WellitonBecker <welliton.lbecker@gmail.com>
2022-10-18 18:37:40 -0300

    Video 13

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,13 +9,20 @@ class NegociacaoController {
         this._negociacoes = new Negociacoes();
         this._negociacoesView = new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView('#mensagemView');
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
         event.preventDefault();
 
         this._negociacoes.adiciona(this._criaNegociacao());
+        this._mensagem._texto = 'Negociação adicionada com sucesso!';
         this._negociacoesView.update(this._negociacoes);
+
+        this._mensagemView.update(this._mensagem);
         this._limparFormulario();
     };
 
